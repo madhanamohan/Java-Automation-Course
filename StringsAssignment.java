2587bcf7bfa44410c4c72a4adf8f61d9e243a259
madhanamohan <madhanamohank4@gmail.com>
2025-06-17 23:48:57 +0530

    Add files via upload

---
+++ StringsAssignment.java
@@ -0,0 +1,42 @@
+package strings;
+
+public class StringsAssignment
+{
+
+	public static void main(String[] args) 
+	{
+		
+/*   //------------------------  palindrom  -------------=//
+	 	String s1="radar";
+		String rev= "";
+		int i;
+		for(i=s1.length()-1;i>=0;i--)
+		{
+			rev=rev+s1.charAt(i);
+		}
+		if(s1.equals(rev))
+		{
+			System.out.println(rev +"\nyes palindrom");
+		}
+		else
+		{
+			System.out.println(rev +"\nnot palindrom");
+		}							*/
+		
+//------------------------  find how many times char is present   -------------=//
+ /*
+		
+		String s3="banana";
+		int i,count=0;
+		char ch ='a';
+		for(i=0;i<s3.length();i++)
+		{
+			if(s3.charAt(i)== ch) 
+			{
+				count++;
+			}
+		}
+		System.out.println("char="+ch+"\ncount ="+count);				*/
+	}
+
+}
